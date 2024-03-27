1ddf8135f1def961e60ccb78cb1eac479aa93f4a
Paula <psanzilundain@gmail.com>
2024-03-27 11:25:16 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/modulo-3-evaluacion-final-psanz22/",
   server: { watch: { usePolling: true } },
 });
